5903adffd083c5d0b7341a95411d183f7a7aa91c
Viktor Zbodulja <viktorzbodulja@gmail.com>
2023-04-15 23:55:05 +0200

    adding colors

---
+++ app.js
@@ -0,0 +1,36 @@
+// Global selections
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll("input[type='range']");
+const currentHexes = document.querySelectorAll(".color h2");
+let initialColors;
+
+//Functions
+
+//Color generator
+function generateHex() {
+  const hexColor = chroma.random();
+  return hexColor;
+}
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+
+    //Add color to the background
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+    //check for contrast
+    checkTextContrast(randomColor, hexText);
+  });
+}
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
+}
+
+randomColors();
