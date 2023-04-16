f60a9e9c9b58b88b37a4d5f6d1e94368659e1c0c
Viktor Zbodulja <viktorzbodulja@gmail.com>
2023-04-16 17:17:04 +0200

    brightness, hue, saturation added on slider

--- app.js
+++ app.js
@@ -22,8 +22,17 @@ function randomColors() {
     hexText.innerText = randomColor;
     //check for contrast
     checkTextContrast(randomColor, hexText);
+    //Initial Colorize Sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
   });
 }
+
 function checkTextContrast(color, text) {
   const luminance = chroma(color).luminance();
   if (luminance > 0.5) {
@@ -31,6 +40,25 @@ function checkTextContrast(color, text) {
   } else {
     text.style.color = "white";
   }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  //scacle saturation
+  const noSaturation = color.set("hsl.s", 0);
+  const fullSaturation = color.set("hsl.s", 1);
+  const scaleSaturation = chroma.scale([noSaturation, color, fullSaturation]); //making palette
+  //Scale brightness
+  const midBrightness = color.set("hsl.l", 0.5);
+  const scaleBrightness = chroma.scale(["black", midBrightness, "white"]);
+
+  //Update input colors
+  saturation.style.backgroundImage = `linear-gradient(to right,${scaleSaturation(
+    0
+  )}, ${scaleSaturation(1)})`;
+  brightness.style.backgroundImage = `linear-gradient(to right,${scaleBrightness(
+    0
+  )}, ${scaleBrightness(0.5)}, ${scaleBrightness(1)})`;
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(255, 0, 0), rgb(255,255 ,0),rgb(0, 255, 0),rgb(0, 255, 255),rgb(0,0,255),rgb(255,0,255),rgb(255,0,0))`;
 }
 
 randomColors();
